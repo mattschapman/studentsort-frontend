f81a5ebb107d0c3f7a12acd62ce1c1877591fa18
mattschapman <mattchapmanmsc@gmail.com>
2026-01-06 15:04:20 +0400

    initial commit

---
+++ app/dashboard/(organizations)/page.tsx
@@ -0,0 +1,10 @@
+import { LogoutButton } from "@/components/auth/logout-button";
+
+export default function Page() {
+    return (
+        <>
+          <p>Organisations Placeholder</p>
+          <LogoutButton />
+        </>
+    )
+}
